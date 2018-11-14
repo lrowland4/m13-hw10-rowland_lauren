04bbb60261f259e1c2b3dfc7f2e7c4efce91718e
Lauren Rowland <Laurenrowland@Laurens-MBP-6.lan>
2018-11-13 19:50:25 -0500

    Add button and section variables

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-//JavaScript Functionalit Part 1 Goes Here. Comment it all out once finsihed.
+//JavaScript Functionality Part 1 Goes Here. Comment it all out once finsihed.
+
+var section = document.getElementByTagName('section');
+
+var button = document.getElementByTagName('button');
 
 
 
 
-//jQuery functionality will go here. Make sure to iclude the document.ready function.
+//jQuery functionality will go here. Make sure to include the document.ready function.
