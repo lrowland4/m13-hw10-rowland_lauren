0f479b974b823e0fe08527296f683a180fdb2c05
Lauren Rowland <Laurenrowland@Laurens-MBP-6.lan>
2018-11-13 20:46:57 -0500

    Comment out javaScript and add jQuerey link back

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 //JavaScript Functionality Part 1 Goes Here. Comment it all out once finsihed.
 
-var section = document.getElementByTagName('section');
+//var section = document.getElementByTagName('section');
 
-var button = document.getElementByTagName('button');
+//var button = document.getElementByTagName('button');
 
-document.getElementByTagName('button').addEventListener('click', function slideToggle(){
-	var section.document.getElementByTagName('section');
-	section.classList.toggle('hide');
-};
+//document.getElementByTagName('button').addEventListener('click', function slideToggle(){
+	//var section.document.getElementByTagName('section');
+	//section.classList.toggle('hide');
+//};
 
 
 
 //jQuery functionality will go here. Make sure to include the document.ready function.
+
