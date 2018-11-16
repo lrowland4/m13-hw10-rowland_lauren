64979179d6d30db581e6b29f7428f01a3d5663b3
Lauren Rowland <Laurenrowland@Laurens-MBP-6.lan>
2018-11-16 13:49:07 -0500

    Add jQuery back

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 //JavaScript Functionality Part 1 Goes Here. Comment it all out once finsihed.
 
-var section = document.getElementsByTagName('section')[0];
+//var section = document.getElementsByTagName('section')[0];
 
-var button = document.getElementsByTagName('button')[0];
+//var button = document.getElementsByTagName('button')[0];
 
-button.addEventListener('click', function slideToggle(){
+//button.addEventListener('click', function slideToggle(){
 	
-	section.classList.toggle('hide');
+	//section.classList.toggle('hide');
+//});
+
+
+
+//jQuery functionality will go here. Make sure to include the document.ready function.
+
+$(document).ready(function(){
+	$('button').click(function(){
+		$('section').slideToggle();
+	});
 });
-
-
-
